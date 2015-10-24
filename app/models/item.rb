2c8c9bfdd9122bf82be4a84fd7d22e9a7b397d5e
OOTANI,Kenji <ootani@virgintech.co.jp>
2015-10-24 13:24:36 +0000

    new branch

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,17 @@ class Item < ActiveRecord::Base
 
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  #====================
+  # Wantしているユーザーの一覧を取得するメソッド
+  #====================
+  has_many :want_to_users, class_name: "Want", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users , through: :wants, source: :user
+  
+  #====================
+  # Haveしているユーザーの一覧を取得するメソッド
+  #====================
+  has_many :have_to_users, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users , through: :haves, source: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  
+  #==================
+  #バリデーション
+  #==================
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,6 +11,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
+  #==================
+  #フォロー／フォロワー
+  #==================
   has_many :following_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following_users, through: :following_relationships, source: :followed
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -14,8 +21,7 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
-
+  
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.create(followed_id: other_user.id)
@@ -29,22 +35,42 @@ class User < ActiveRecord::Base
     following_users.include?(other_user)
   end
 
-  ## TODO 実装
-  def have(item)
-  end
+  #====================
+  # Wantしているアイテムの一覧を取得するメソッド
+  #====================
+  has_many :want_to_items, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
 
-  def unhave(item)
-  end
-
-  def have?(item)
-  end
-
+  # アイテムをWantする
   def want(item)
+    want_to_items.create(item_id: item.id)
   end
-
+  # Wantを解除する
   def unwant(item)
+    want_to_items.find_by(item_id: item.id).destroy
+  end
+  # Wantしてるかどうか？
+  def want?(item)
+    want_items.include?(item)
   end
 
-  def want?(item)
+  #====================
+  # Haveしているアイテムの一覧を取得するメソッド
+  #====================
+  has_many :have_to_items, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
+
+  # アイテムをHaveする
+  def have(item)
+    have_to_items.create(item_id: item.id)
   end
+  # Haveを解除する
+  def unhave(item)
+    have_to_items.find_by(item_id: item.id).destroy
+  end
+  # Haveしてるかどうか？
+  def have?(item)
+    have_items.include?(item)
+  end
+  
 end
